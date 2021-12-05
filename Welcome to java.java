4e9e801c4400435770c66de6067924c587b10351
Swapnil Singhal <77186164+swapnil46@users.noreply.github.com>
2021-12-05 23:14:30 +0530

    Create Welcome to java.java

---
+++ Welcome to java.java
@@ -0,0 +1,10 @@
+# HackerRank problem solved using Java
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
