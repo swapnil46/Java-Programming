3c03077f2627f08b70cc0c1402a7e0e2c458c7f2
Swapnil Singhal <77186164+swapnil46@users.noreply.github.com>
2021-12-11 21:51:23 +0530

    Create Java Output Formatting.java

---
+++ Java Output Formatting.java
@@ -0,0 +1,24 @@
+#Java Output Formatting
+
+#Hackerrank problem solved using java.
+
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+         Scanner sc=new Scanner(System.in);
+                System.out.println("================================");
+                for(int i=0;i<3;i++)
+                {
+                    String s1=sc.next();
+                    int x=sc.nextInt();
+                    System.out.printf( "%-15s%03d %n", s1, x);
+                }
+                System.out.println("================================");
+
+    }
+}
